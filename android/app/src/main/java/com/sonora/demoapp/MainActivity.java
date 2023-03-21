9737f984594348a9d6d2907672a1e5903f557952
nvidwans <neel@sonorasoftware.com>
2023-03-21 17:03:40 +0530

    Initial commit

---
+++ android/app/src/main/java/com/sonora/demoapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sonora.demoapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sonora.demoapp',
+  appName: 'demo-deploy',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
